52a0644d598103ac39f72a2bbea8da67800455cf
firearian <firearian@gmail.com>
2017-09-22 01:57:41 +1200

    Add files via upload
    Fixed Student Stats barring a few exceptional math situations

--- From Gator/graphdata.php
+++ From Gator/graphdata.php
@@ -137,6 +137,125 @@ answers ON answers.Qid=questions.id WHERE questions.Qname='$data'");
 
     ksort($temp,SORT_NUMERIC);
     $answer["keys"] = $temp;
+
+} elseif ($method=="student"){
+
+    $stats = array();
+    $tempvalues = array();
+    $calculatingvalues = array();
+    $N = 0;
+    $sigma = 0;
+
+    $data = $_REQUEST['Tid'];
+    $stmt = $pdo->prepare("SELECT DISTINCT Correct FROM testsubmissions WHERE Tid='$data'");
+    $results = $stmt->execute();
+    $percentagevalues = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    for ($i = 0; $i < sizeof($percentagevalues); $i++){
+        $tempvalues[(string)($percentagevalues[$i]['Correct']*100)."%"] = 0;
+    }
+
+    $stmt = $pdo->prepare("SELECT id, Correct FROM testsubmissions WHERE Tid='$data'");
+    $results = $stmt->execute();
+    $j = 0;
+
+    while($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $j++;
+        for ($i = 0; $i < sizeof($percentagevalues); $i++){
+            if ($result['Correct'] == $percentagevalues[$i]['Correct']){
+                $tempvalues[(string)($percentagevalues[$i]['Correct']*100)."%"]++;
+                $sigma = $sigma + $percentagevalues[$i]['Correct']*100;
+                $calculatingvalues[$j] = $percentagevalues[$i]['Correct']*100;
+                $N++;
+            }
+        }
+    }
+
+    $stats["mean"] = $sigma/$N;
+    $stats["mode"] = array_keys($tempvalues, max($tempvalues))[0];
+
+    asort($calculatingvalues,SORT_NUMERIC);
+    $calculatingvalues= array_values($calculatingvalues);
+
+    $stats["median"] = checkvalue($calculatingvalues, 1, $N);
+
+    if ($N < 4) {
+        $stats["lquartile"] = "N/A";
+        $stats["uquartile"] = "N/A";
+    } else {
+        $stats["lquartile"] = checkvalue($calculatingvalues, 2, $N);
+        $stats["uquartile"] = checkvalue($calculatingvalues, 3, $N);
+    }
+
+    $uname = $_SESSION['username'];
+    $stmt = $pdo->prepare("SELECT DISTINCT Correct FROM testsubmissions WHERE Username='$uname' AND Tid='$data'");
+    $results = $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    $stats["score"] = $result["Correct"]*100;
+
+
+    $answer["stats"] = $stats;
+    ksort($tempvalues,SORT_NUMERIC);
+    $answer["values"] = $tempvalues;
+
+}
+
+function odd_array(array $calculatingvalues, $ovalue){
+
+    $val1 = floor($ovalue);
+    $val2 = ceil($ovalue);
+    $total = 0;
+    $count = 1;
+
+        for ($i = 0; $i < sizeof($calculatingvalues); $i++){
+            if ($count == $val1 OR $count == $val2){
+                $total = $total + $calculatingvalues[$i];
+            }
+            $count++;
+        }
+        if ($ovalue > 1) {
+            return ($total*($ovalue-round($ovalue, 0,PHP_ROUND_HALF_DOWN)));
+        } else {
+            return ($total*$ovalue);
+        }
+}
+
+function even_array(array $calculatingvalues, $ovalue){
+    $count = 1;
+        for ($i = 0; $i < sizeof($calculatingvalues); $i++){
+            if ($count == $ovalue){
+                return $calculatingvalues[$i];
+            }
+            $count++;
+        }
+}
+
+function checkvalue(array $calculatingvalues, $mode, $val){
+    if ($mode == 1){
+        if ($val%2 or $val==2){
+            $ans = odd_array($calculatingvalues, $val/2);
+            return ($ans === null) ? 0 : $ans;
+        } else{
+            $ans = even_array($calculatingvalues, $val/2);
+            return ($ans === null) ? 0 : $ans;
+        }
+    } elseif ($mode == 2){
+        if ($val%4){
+            $ans = odd_array($calculatingvalues, $val/4);
+            return ($ans === null) ? 0 : $ans;
+        } else{
+            $ans = even_array($calculatingvalues, $val/4);
+            return ($ans === null) ? 0 : $ans;
+        }
+    } elseif ($mode == 3){
+        if (($val*3)%4){
+            $ans = odd_array($calculatingvalues, ($val*3)/4);
+            return ($ans === null) ? 0 : $ans;
+        } else{
+            $ans = even_array($calculatingvalues, ($val*3)/4);
+            return ($ans === null) ? 0 : $ans;
+        }
+    }
 }
 
 
